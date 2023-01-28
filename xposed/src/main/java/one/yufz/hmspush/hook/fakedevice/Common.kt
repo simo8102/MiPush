105999e826f8c4a50f9b2128ce57233168ea40e4
Nihility <Nihility.t@gmail.com>
2023-01-28 19:20:07 +0800

    fake class miui.os.Build

--- xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/Common.kt
+++ xposed/src/main/java/one/yufz/hmspush/hook/fakedevice/Common.kt
@@ -2,6 +2,7 @@ package one.yufz.hmspush.hook.fakedevice
 
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import one.yufz.hmspush.hook.XLog
+import one.yufz.xposed.hookMethod
 
 open class Common : IFakeDevice {
     companion object {
@@ -11,6 +12,41 @@ open class Common : IFakeDevice {
     override fun fake(lpparam: XC_LoadPackage.LoadPackageParam): Boolean {
         XLog.d(TAG, "fake() called with: packageName = ${lpparam.packageName}")
         fakeAllBuildInProperties()
+        fakeClass(lpparam)
         return true
+    }
+
+    private fun fakeClass(lpparam: XC_LoadPackage.LoadPackageParam) {
+        var isMIUI = false
+        try {
+            // check MIUI environment
+            Class.forName("miui.os.Build", false, lpparam.classLoader)
+            isMIUI = true
+        } catch (_: Throwable) {
+        }
+        if (isMIUI) {
+            return
+        }
+
+        val classMap: Map<String, Class<out Any>> = mapOf(
+            "miui.os.Build" to Object::class.java,
+        )
+        Class::class.java.hookMethod(
+            "forName",
+            String::class.java,
+            Boolean::class.java,
+            ClassLoader::class.java
+        ) {
+            doBefore {
+                var requestClass = args[0]
+                val returnClass = classMap[requestClass]
+                if (returnClass != null) {
+                    XLog.d(TAG, "forHook $requestClass")
+                    result = returnClass
+                } else {
+                    XLog.d(TAG, "forName $requestClass")
+                }
+            }
+        }
     }
 }
